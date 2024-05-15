6cbcb77ad69cdd9dc4da0152d70b1b3c00166087
Patrick Coxall <mr.coxall@mths.ca>
2024-05-15 15:08:40 +0000

    page update with Hello, World

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -38,6 +38,10 @@ export default function Home() {
           priority
         />
       </div>
+      <div>
+      <br />
+        <p>Hello, World!</p>
+      </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
